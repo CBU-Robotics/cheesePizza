34f42dcd6d38ee52a1c8a78f9a8f3a52ffb4b2a3
MichaelPendleton <michaelcurtis.pendleton@calbaptist.edu>
2023-10-14 13:42:15 -0700

    8000 Max voltage

--- include/main.h
+++ include/main.h
@@ -75,8 +75,6 @@ void opcontrol(void);
 /**
  * You can add C++-only headers here
  */
-#include "subsystemHeaders/drive.hpp"
-#include "subsystemHeaders/globals.hpp"
 //#include <iostream>
 #endif
 
